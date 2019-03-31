b9f8f67d5767a927130dbb2cf526fcb768284765
Zonayed Ahmed <zonayedpca@yahoo.com>
2019-03-31 12:18:23 +0530

    project done

---
+++ app.js
@@ -0,0 +1,10 @@
+const five = require('johnny-five');
+const board = new five.Board();
+
+board.on('ready', function() {
+  const led = new five.Led(13);
+  led.blink(500);
+  this.repl.inject({
+    led
+  });
+});
